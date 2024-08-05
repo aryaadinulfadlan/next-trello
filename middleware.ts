cecdbb5047b937ea22721c0cd003308ba58bc551
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-08-05 12:29:43 +0700

    add organization

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,30 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtected = createRouteMatcher(["/dashboard"]);
+const isProtected = createRouteMatcher([
+  "/dashboard",
+  "/select-org",
+  // "/organization/:id",    /organization/id
+  "/organization(.*)", //organization/apapun/setelahini/icludes/only/organization
+]);
 export default clerkMiddleware((auth, req) => {
   if (isProtected(req)) auth().protect();
+  if (auth().userId && !isProtected(req)) {
+    let path = "/select-org";
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/select-org"
+  ) {
+    const orgSelection = new URL("/select-org", req.url);
+    return NextResponse.redirect(orgSelection);
+  }
 });
 
 export const config = {
