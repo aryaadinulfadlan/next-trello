99b245bc55c201046861ac779178231d2af837eb
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-08-07 16:29:33 +0700

    try server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,18 @@
-export default function OrganizationIdPage() {
-  return <div>Organization Page</div>;
+import { db } from "@/lib/db";
+import Form from "./_components/form";
+import Board from "./_components/board";
+
+export default async function OrganizationIdPage() {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} id={board.id} title={board.title} />
+        ))}
+      </div>
+    </div>
+  );
 }
