93a968f1301eb18b87cf70036ba8deb7808a8e33
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-08-10 22:57:35 +0700

    adding specific board page

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,56 @@
+import { db } from "@/lib/db";
+import { ILayoutWithParams } from "@/types/root";
+import { auth } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import BoardNavbar from "./_components/board-navbar";
+
+export async function generateMetadata({
+  params,
+}: Pick<ILayoutWithParams<{ boardId: string }>, "params">) {
+  const { orgId } = auth();
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+  return {
+    title: board?.title || "Board",
+  };
+}
+
+export default async function BoardIdLayout({
+  children,
+  params,
+}: ILayoutWithParams<{ boardId: string }>) {
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+  if (!board) {
+    notFound();
+  }
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${board.imageFullUrl})`,
+      }}
+      className="relative h-full bg-no-repeat bg-center bg-cover"
+    >
+      <BoardNavbar board={board} />
+      <div className="absolute inset-0 bg-black/20" />
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+}
--- types/root.ts
+++ types/root.ts
@@ -3,3 +3,13 @@ import { ReactNode } from "react";
 export interface ILayout {
   children: ReactNode;
 }
+
+export interface ILayoutWithParams<T> {
+  children: ReactNode;
+  params: T;
+}
+
+export interface IPageWithParams<TParams, TSearchParams> {
+  params: TParams;
+  searchParams: TSearchParams;
+}
